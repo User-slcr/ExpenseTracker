2107a473888407d9e2ec40badc07ac2a622d2339
Liton Chandro <118765916+User-slcr@users.noreply.github.com>
2024-09-18 01:06:24 +0600

    Add files via upload

---
+++ src/main/java/com/example/expensetracker/Model/Expense.java
@@ -0,0 +1,40 @@
+package com.example.expensetracker.Model;
+
+public class Expense {
+    public int Id;
+    public String Item, Amount;
+
+    public Expense(int Id, String Item, String Amount, String Date){
+        this.Id=Id;
+        this.Item=Item;
+        this.Amount=Amount;
+    }
+
+    public Expense()
+    {}
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getItem() {
+        return Item;
+    }
+
+    public void setItem(String item) {
+        Item = item;
+    }
+
+    public String getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(String amount) {
+        Amount = amount;
+    }
+
+}
